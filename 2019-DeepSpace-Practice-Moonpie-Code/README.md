3e325d5200627ca76ad938e993af485e5563cb4a
Rylan Meilutis <meilutisrt@gmail.com>
2019-10-15 20:11:08 -0400

    Repo has been fixed

--- 2019-DeepSpace-Practice-Moonpie-Code/README.md
+++
@@ -1,2 +0,0 @@
-# 2019-DeepSpace
-The current repository for the 2019 game Destination: Deep Space
---
+++ src/main/java/org/usfirst/frc/team1559/robot/Robot.java
@@ -0,0 +1,72 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team1559.robot;
+
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import org.usfirst.frc.team1559.robot.OperatorInterface;
+
+
+public class Robot extends TimedRobot
+{
+	//instantiate variables and other things 
+
+	@Override
+	public void robotInit()
+	{
+		
+		// Sub-System Instantiations
+		
+		}
+
+	@Override
+	public void robotPeriodic()
+	{
+
+	}
+
+	@Override
+	public void autonomousInit()
+	{
+		
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+
+	@Override
+	public void teleopInit()
+	{
+		
+
+
+	}
+
+
+	@Override
+	public void teleopPeriodic()
+	{
+	
+	
+	}
+
+	@Override
+	public void disabledInit()
+	{
+	
+	}
+
+	@Override
+	public void disabledPeriodic()
+	{
+	
+	}
+}
